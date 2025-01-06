0d38ed8048ad8fb36b975827becb8b68d73e8a6b
HugoSgnl <144049621+HugoSgnl@users.noreply.github.com>
2025-01-06 13:19:55 +0100

    Update LICENSE.R

--- LICENSE.R
+++ LICENSE.R
@@ -1,3 +1,6 @@
+This tool as a whole is distributed under GPL-3 (GNU GENERAL PUBLIC
+LICENSE version 3).
+
 Copyright (C) 2025 Sebastià Barceló, Hugo Signol
 
 JSON Dataset Viewer - A powerful and user-friendly Shiny application for
